74d4c93fc794a14458414a22b1fa09b8503ee067
Donald E. Llopis <machinezilla@gmail.com>
2011-07-19 08:30:43 -0400

    Started converting flyer code to use Flyer object.

--- enemy_patterns.js
+++ enemy_patterns.js
@@ -7,35 +7,6 @@ var half_canvasWidth;
 var dateTemp;
 var lastTime;
 
-$(document).ready(function() {
-	canvas = $("#myCanvas");
-	context = canvas.get(0).getContext("2d");
-	canvasHeight = canvas.height();
-	canvasWidth = canvas.width();
-	half_canvasHeight = canvasHeight / 2;
-	half_canvasWidth = canvasWidth / 2;
-	dateTemp = new Date();
-	lastTime = dateTemp.getTime();
-	animate();
-});
-
-
-function drawRect(x, y, width, height) {
-	x -= width / 2;
-	y -= height / 2;
-	context.fillRect(x, y, width, height);
-}
-
-
-function drawFlyer(x, y, theta, color) {
-	context.save();
-	context.fillStyle = color;
-	context.translate(x + half_canvasWidth, y + half_canvasHeight);
-	context.rotate(theta);
-	drawRect(0, 0, 10, 10);
-	context.restore();
-}
-
 var x0 = 0;
 var y0 = 0;
 var x1 = 0;
@@ -43,45 +14,103 @@ var y1 = 0;
 var dt = 0;
 var r = 20.0;
 
+var fliers;
+var f0;
+var f1;
+
+$(document).ready(function() {
+	canvas = $("#myCanvas");
+	context = canvas.get(0).getContext("2d");
+	canvasHeight = canvas.height();
+	canvasWidth = canvas.width();
+	half_canvasHeight = canvasHeight / 2;
+	half_canvasWidth = canvasWidth / 2;
+	init();
+	animate();
+});
+
+
+function Flyer(startX, startY, startTheta, color, v, r) {
+	
+	this.x = startX;
+	this.y = startY;
+	this.theta = startTheta;
+	this.color = color;
+	this.r = r;
+	this.v = v;
+	this.cosine = Math.cos(this.theta);
+	this.sine = Math.sin(this.theta);
+
+	this.move = function() {
+		this.cosine = Math.cos(this.theta);
+		this.sine = Math.sin(this.theta);
+		this.x = this.v * this.cosine + this.x;
+		this.y = this.v * this.sine + this.y;
+
+		if (this.x > half_canvasWidth) {
+			this.x = -half_canvasWidth;
+		}
+		else if (this.x < -half_canvasWidth) {
+			this.x = half_canvasWidth;
+		}
+
+		if (this.y > half_canvasHeight) {
+			this.y = -half_canvasHeight;
+		}
+		else if (this.y < -half_canvasHeight) {
+			this.y = half_canvasHeight;
+		}
+
+		if ((Math.random() * 100) < 60) {
+			if ((Math.random() * 10) < 5) {
+				this.theta += Math.PI / 32;
+			}
+			else {
+				this.theta -= Math.PI / 32;
+			}
+		}
+	}
+
+	this.mirror = function(leader) {
+		this.theta = leader.theta;
+		this.x = r * -leader.sine + leader.x;
+		this.y = r * leader.cosine + leader.y;
+	}
+
+	this.drawCenteredRect = function(x, y, w, h) {
+		x -= w / 2;
+		y -= h / 2;
+		context.fillRect(x, y, w, h);
+	}
+
+	this.draw = function() {
+		context.save();
+		context.fillStyle = color;
+		context.translate(this.x + half_canvasWidth,
+			       	this.y + half_canvasHeight);
+		context.rotate(this.theta);
+		this.drawCenteredRect(0, 0, 10, 10);
+		context.restore();
+	}
+}
+
+function init() {
+	dateTemp = new Date();
+	lastTime = dateTemp.getTime();
+	f0 = new Flyer(0, 0, 0, "rgb(255, 0, 0)", 10, 0);
+	f1 = new Flyer(0, 20, 0, "rgb(255, 255, 0)", 10, 20);
+}
+
 function animate() {
 
 	context.fillStyle = "rgb(0, 0, 0)";
 	context.fillRect(0, 0, canvasWidth, canvasHeight);
 
-	drawFlyer(x0, y0, dt, "rgb(255, 0, 0)");
-	drawFlyer(x1, y1, dt, "rgb(255, 255, 0)");
+	f0.draw();
+	f1.draw();
 
-	var c = Math.cos(dt);
-	var s = Math.sin(dt);
-
-	x0 = 10.0 * c + x0;
-	y0 = 10.0 * s + y0;
-
-	x1 = r * -s + x0;
-	y1 = r * c + y0;
-
-	if (x0 > half_canvasWidth) {
-		x0 = -half_canvasWidth;
-	}
-	else if (x0 < -half_canvasWidth) {
-		x0 = half_canvasWidth;
-	}
-
-	if (y0 > half_canvasHeight) {
-		y0 = -half_canvasHeight;
-	}
-	else if (y0 < -half_canvasHeight) {
-		y0 = half_canvasHeight;
-	}
-
-	if ((Math.random() * 100) < 60) {
-		if ((Math.random() * 10) < 5) {
-			dt += Math.PI / 32;
-		}
-		else {
-			dt -= Math.PI / 32;
-		}
-	}
+	f0.move();
+	f1.mirror(f0);
 
 	setTimeout(animate, 33);
 }
